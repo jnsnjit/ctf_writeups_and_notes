3b3a6cd0c624cce228c5d3b2333f08a10fc4d7ba
jnsnjit <156932213+jnsnjit@users.noreply.github.com>
2024-10-31 12:57:21 -0400

    more on perl chall

--- crypto/japh.pl
+++ crypto/japh.pl
@@ -1,1 +1,38 @@
-IyEvdXNyL2Jpbi9wZXJsCiR8ID0gMTtwcmludCAiV2VsY29tZSB0byB0aGUgaG91c2Ugb2YgdGhlIHNow6huIVxuIjtwcmludCAiRmluZCB0aGUgd2lzZG9tIHRocm91Z2ggdGhlIFBlcmwuICBDb250cm9sIGlzIGtleS5cbiI7(HEYMAYA)aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1meFItclRlX2FoRQ==bXkgJHMgPSA8U1RESU4+O2Nob21wKCRzKTskcyA9fiB5LzAtOUEtWmEtei8wLTlBLVphLXovcztpZiAobGVuZ3RoKCRzKSA8MjE2KSB7cHJpbnQoIkNlYXNlIHlvdXIgbWVkZGxpbmchIFRoaXMgd2lsbCBiZSB5b3VyIHRvbWIhXG4iKTtleGl0O31teSAkYiA9IHN1YnN0cigkcywgJHMtMSwgJHMpOyRiID0gc3Vic3RyKCRiLCAkYisxLCAkYik7JGIgPX4gcy9SLzAvZzskYiA9fiBzLyhbSC1TXSkvXGM4L2c7JGIgPX4gcy8oW2ktdF0pL3VjKCQxKS9nZTskYiA9fiBzL3gvUi9nZTtteSAkZCA9ICRiOyRkID1+IHkvNC1wL0Etey87bXkgJGZpbGUgPSAnamFwaC50eHQnO29wZW4oRkgsICc8JywgJGZpbGUpIG9yIGRpZSAkITskZiA9IDxGSD47aWYoJGYgZXEgJGQpIHskZmlsZSA9ICdmbGFnLnR4dCc7b3BlbihGSCwgJzwnLCAkZmlsZSkgb3IgZGllICQhO3doaWxlKDxGSD4pIHtwcmludCAkXzt9fQo=
+#!/usr/bin/perl
+$| = 1;print "Welcome to the house of the shÃ¨n!\n";
+print "Find the wisdom through the Perl.  Control is key.\n";
+https://www.youtube.com/watch?v=fxR-rTe_ahEmy 
+
+#$s is user input, stdin function, similiar to c
+$s = <STDIN>;chomp($s);
+#this regex command takes the user input, and does "smashing, which takes duplicate
+#chars next to each and remove duplicates"
+$s =~ y/0-9A-Za-z/0-9A-Za-z/s;
+
+#char length must be greater then 216
+if (length($s) < 216) {print("Cease your meddling! This will be your tomb!\n");
+    exit;
+}
+#takes string and performs this
+my $b = substr($s, $s-1, $s);
+#takes substr input, finds number (we want at 128 chars, so minus one --> type 129)
+$b = substr($b, $b+1, $b);
+#then takes from that 128, we want 64, but plus one, so type 63)
+
+#four regex functions, only really removes and replaces values that arent relevant
+$b =~ s/R/0/g;$b =~ s/([H-S])/\c8/g;
+$b =~ s/([i-t])/uc($1)/ge;
+$b =~ s/x/R/ge;
+my $d = $b;
+#performs text ROT of 13 chars
+$d =~ y/4-p/A-{/;
+
+#opens answer file, compares, if correct, print flag
+my $file = 'japh.txt';
+open(FH, '<', $file) or die $!;
+$f = <FH>;
+if($f eq $d) {
+    $file = 'flag.txt';
+    open(FH, '<', $file) or die $!;
+    while(<FH>) {print $_;}
+}
